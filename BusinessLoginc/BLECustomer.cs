aaa8e06de0ca2506bdfc90ad88d1f7a82222e818
markobozic346 <markobozic346@gmail.com>
2024-04-05 19:32:30 +0200

    Add business logic

---
+++ BusinessLoginc/BLECustomer.cs
@@ -0,0 +1,108 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace BusinessLoginc
+{
+    public class BLECustomer
+    {
+
+        private static BLECustomer instance = null;
+
+        public static BLECustomer Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new BLECustomer();
+                }
+                return instance;
+
+            }
+        }
+
+        public Customers c = Customers.Instance;
+
+        private Customer Convert(DataRow dr)
+        {
+            Customer c = new Customer();
+
+            c.Custid = Int32.Parse(dr["custid"].ToString());
+            c.Companyname = dr["companyname"].ToString();
+            c.Contacttitle = dr["contacttitle"].ToString();
+            c.Contactname = dr["contactname"].ToString();
+            c.Address = dr["address"].ToString();
+            c.City = dr["city"].ToString();
+            c.Region = dr["region"].ToString();
+            c.Postcode = dr["postalcode"].ToString();
+            c.Country = dr["country"].ToString();
+            c.Phone = dr["Phone"].ToString();
+            c.Fax = dr["Fax"].ToString();
+
+            return c;
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            return Convert(c.dtCustomers.Select("custid = " + id.ToString())[0]);
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            List<Customer> l = new List<Customer>();
+
+            foreach(DataRow dr in c.dtCustomers.Rows)
+            {
+                l.Add(Convert(dr));
+            }
+
+            return l;
+        }
+
+        public List<Customer> GetCustomers(string search)
+        {
+            List<Customer> l = new List<Customer>();
+
+            foreach (DataRow dr in c.dtCustomers.Rows)
+            {
+                if (dr["contactname"].ToString().ToLower().StartsWith(search.ToLower()) ||
+                    dr["companyname"].ToString().ToLower().StartsWith(search.ToLower()) ||
+                    dr["contacttitle"].ToString().ToLower().StartsWith(search.ToLower()) ||
+                    dr["city"].ToString().ToLower().StartsWith(search.ToLower()))
+                {
+                    l.Add(Convert(dr));
+                }
+            }
+            return l;
+        }
+
+        public bool UpdateCustomer(Customer cu)
+        {
+            c.Update(cu);
+            return true;
+        }
+        public bool InsertCustomer(Customer cu)
+        {
+            c.Insert(cu);
+            return true;
+        }
+
+        public bool DeleteCustomer(int id)
+        {
+            c.Delete(id);
+            return true;
+        }
+
+        public DataTable GetCustomersTableData()
+        {
+            return Customers.Instance.GetCustomersTableData();
+        }
+
+    }
+}
---
+++ BusinessLoginc/BLEmployee.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using DataLayer;
+using Domain;
+
+namespace BusinessLoginc
+{
+    public class BLEmployee
+    {
+        private static BLEmployee instance = null;
+
+        public static BLEmployee Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new BLEmployee();
+                }
+                return instance;
+            }
+        }
+
+        public Employees e = Employees.Instance;
+        private Employee Convert(DataRow dr)
+        {
+            Employee e = new Employee();
+            e.Empid = Int32.Parse(dr["empid"].ToString());
+            e.Lastname = dr["lastname"].ToString();
+            e.Firstname = dr["firstname"].ToString();
+            e.Title = dr["title"].ToString();
+            e.Titleofcourtesy = dr["titleofcourtesy"].ToString();
+            e.Birthdate = DateTime.Parse(dr["birthdate"].ToString());
+            e.Hiredate = DateTime.Parse(dr["hiredate"].ToString());
+            e.Address = dr["address"].ToString();
+            e.City = dr["city"].ToString();
+            e.Region = dr["region"].ToString();
+            e.Postalcode = dr["postalcode"].ToString();
+            e.Country = dr["country"].ToString();
+            e.Phone = dr["phone"].ToString();
+
+            int mgrid;
+            int.TryParse(dr["mgrid"].ToString(), out mgrid);
+            e.Mgrid = mgrid;
+
+            return e;
+        }
+        public Employee GetEmployee(int id)
+        {
+            return Convert(e.dtEmployees.Select("empid = " + id.ToString())[0]);
+        }
+        public List<Employee> GetEmployees(string search)
+        {
+            List<Employee> l = new List<Employee>();
+            foreach (DataRow dr in e.dtEmployees.Rows)
+            {
+                if (dr["firstname"].ToString().ToLower().StartsWith(search.ToLower()) ||
+                    dr["lastname"].ToString().ToLower().StartsWith(search.ToLower()))
+                {
+                    l.Add(Convert(dr));
+                }
+            }
+
+            return l;
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> l = new List<Employee>();
+
+            foreach (DataRow dr in e.dtEmployees.Rows)
+            {
+                l.Add(Convert(dr));
+            }
+
+            return l;
+        }
+
+        public bool UpdateEmployee(Employee em)
+        {
+            e.Update(em);
+            return true;
+        }
+
+        public bool InsertEmployee(Employee em)
+        {
+            e.Insert(em);
+            return true;
+        }
+
+        public bool DeleteEmployee(int id)
+        {
+            e.Delete(id);
+            return true;
+        }
+
+        public DataTable GetEmployeesTableData()
+        {
+            return Employees.Instance.GetEmployeeTableData();
+        }
+
+    }
+}
