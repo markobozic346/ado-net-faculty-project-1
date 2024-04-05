50b37a3f132844e9924cc3d0785bec297cd310b7
markobozic346 <markobozic346@gmail.com>
2024-04-05 19:30:40 +0200

    Add domain layer

---
+++ Domain/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Customer
+    {
+        private int custid;
+        private String companyname;
+        private String contacttitle;
+        private String contactname;
+        private String address;
+        private String city;
+        private String region;
+        private String postcode;
+        private String country;
+        private String phone;
+        private String fax;
+
+
+        public int Custid { get; set; }
+        public String Companyname { get; set; }
+        public String Contacttitle { get; set; }
+        public String Contactname { get; set; }
+        public String Address { get; set; }
+        public String City { get; set; }
+        public String Region { get; set; }
+        public String Postcode { get; set; }
+        public String Country { get; set; }
+        public String Phone { get; set; }
+        public String Fax { get; set; }
+
+
+       
+    }
+}
+
---
+++ Domain/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Employee
+    {
+        private int empid;
+        private String lastname;
+        private String firstname;
+        private String title;
+        private String titleofcourtesy;
+        private DateTime? birthdate;
+        private DateTime? hiredate;
+        private String address;
+        private String city;
+        private String region;
+        private String postalcode;
+        private String country;
+        private String phone;
+        private int mgrid;
+
+        public int Empid { get; set; }
+        public String Lastname { get; set; }
+        public String Firstname { get; set; }
+        public String Title { get; set; }
+        public String Titleofcourtesy { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public DateTime? Hiredate { get; set; }
+        public String Address { get; set; }
+        public String City { get; set; }
+        public String Region { get; set; }
+        public String Postalcode { get; set; }
+        public String Country { get; set; }
+        public String Phone { get; set; }
+        public int Mgrid { get; set; }
+
+
+
+    }
+}
